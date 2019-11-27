7624119266334adb534e4f9063ce5ba15b84cb2a
acake4 <58260765+acake4@users.noreply.github.com>
2019-11-27 14:59:23 +0100

    Uploading python script

--- serialHCSR04.py
+++ serialHCSR04.py
@@ -1,7 +1,8 @@
-import serial
+import serial #module needed for serial communication
 
-ser = serial.Serial('COM6', baudrate = 9600, timeout = 1)
+ser = serial.Serial('COM6', baudrate = 9600, timeout = 1) #opening serial communication, port->COM6, baudrate->9600, timeout->1
 
+#function if we want to capture HCSR04 readings
 def getValues():
     ser.write(b'g')
     arduinoData = ser.readline().decode('ascii')
